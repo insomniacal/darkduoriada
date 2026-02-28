d11662ac2535776a7488eba0c0bebd1e98a4945c
dzigocu <rybnikovmark795@gmail.com>
2026-02-28 18:44:00 +0300

    git# Please enter the commit message for your changes. Lines starting

--- anything.py
+++ anything.py
@@ -1,1 +1,17 @@
-print("я ебал твою мать")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = "8671339317:AAGKQJd0LXGVOh-aJfqo3PIGhn76agzPb5o"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет. Я работаю.")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Ты написал: {update.message.text}")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+app.run_polling()
