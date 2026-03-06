caa25527c0b9d857a43a27168214ba52f923f8ce
insomnia <insomniacal6@gmail.com>
2026-03-06 22:32:24 +0100

    ХЕЕЕЕЕЕЕЕЛП КОМАНДА

--- anything.py
+++ anything.py
@@ -2,14 +2,15 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8671339317:AAGKQJd0LXGVOh-aJfqo3PIGhn76agzPb5o"
-# команда /start
+
+#/start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Бот работает 🚀n/n")
- "Напиши /help чтобы увидеть список команд."
+    await update.message.reply_text(
+        "здравствуйте, вас приветствует бот который может послать вас куда подальше а именно нахуй.\n\n"
+        "Напиши /help дабы узнать список комманд для дальнейших действий"
     )
 
-
-# команда /help
+#/help
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Список команд:\n\n"
@@ -17,26 +18,11 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/help — показать список команд"
     )
 
-
-# создание приложения
 app = ApplicationBuilder().token(TOKEN).build()
 
-# обработчики команд
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help_command))
 
 print("Бот запущен...")
 
-# запуск бота
-app.run_polling()
-
-# создание приложения
-app = ApplicationBuilder().token(TOKEN).build()
-
-# добавляем команду
-app.add_handler(CommandHandler("start", start))
-
-print("Бот запущен...")
-
-# запуск
 app.run_polling()
